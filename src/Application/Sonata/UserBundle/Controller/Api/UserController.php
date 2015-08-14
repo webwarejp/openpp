437382d1787735f556c76783730a57abf2719568
Yoshihiro Taniguchi <taniguchi@webware.co.jp>
2015-08-14 07:04:02 -0400

    user api の更新。apidocの不具合修正。

--- src/Application/Sonata/UserBundle/Controller/Api/UserController.php
+++ src/Application/Sonata/UserBundle/Controller/Api/UserController.php
@@ -7,19 +7,20 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Sonata\UserBundle\Model\UserInterface;
 use FOS\RestBundle\View\View;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Sonata\UserBundle\Controller\Api\UserController as BaseUserController;
 
-class UserController extends ContainerAware
+class UserController extends BaseUserController
 {
     /**
      * @ApiDoc(
-     *  description="Retrieves a specific user"
+     *  description="Retrieves a specific user",
      * )
      */
     public function getMeAction()
     {
-        $tokenManager = $this->container->get('fos_oauth_server.access_token_manager.default');
+        $tokenManager = $this->get('fos_oauth_server.access_token_manager.default');
         $accessToken = $tokenManager->findTokenByToken(
-            $this->container->get('security.context')->getToken()->getToken()
+            $this->get('security.context')->getToken()->getToken()
         );
         $me = $accessToken->getUser();
 
